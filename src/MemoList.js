510f49c8d37e215b7f79f842e520c4aa32f6648c
hirokiej <123009866+hirokiej@users.noreply.github.com>
2025-03-11 22:54:33 +0900

    メモ一覧を１行目だけ表示するようにしました

--- src/MemoList.js
+++ src/MemoList.js
@@ -10,19 +10,22 @@ export default function MemoList({
 }) {
   return (
     <>
-      {memos.map((memo) => (
-        <div
-          key={memo.id}
-          className={memo.id === selectedId ? "selected-memo" : "other-memos"}
-          onClick={() => {
-            setInput(memo.memo);
-            setIsEditing(true);
-            setSelectedId(() => memo.id);
-          }}
-        >
-          {memo.memo}
-        </div>
-      ))}
+      {memos.map((memo) => {
+        const firstLine = memo.memo.split("\n")[0];
+        return (
+          <div
+            key={memo.id}
+            className={memo.id === selectedId ? "selected-memo" : "other-memos"}
+            onClick={() => {
+              setInput(memo.memo);
+              setIsEditing(true);
+              setSelectedId(() => memo.id);
+            }}
+          >
+            {firstLine}
+          </div>
+        );
+      })}
       <div
         onClick={() => {
           const newMemo = { id: uuidv4(), memo: "新規メモ" };
